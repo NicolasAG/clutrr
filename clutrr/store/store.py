9ad0191f398bfbcbf3fc7c3ce15e59fd87837c2e
Nicolas Gontier <nicolas.gontier@element.ai>
2019-10-08 02:51:32 +0000

    added question store

--- clutrr/store/store.py
+++ clutrr/store/store.py
@@ -11,18 +11,21 @@ import os
 import json
 import yaml
 
+
 class Store:
-    def __init__(self,args):
+    def __init__(self, args):
         attribute_store = args.attribute_store if args.attribute_store else 'attribute_store.json'
         relations_store = args.relations_store if args.relations_store else 'relations_store.json'
+        question_store = args.question_store if args.question_store else 'question_store.json'
         rules_store = args.rules_store if args.rules_store else 'rules_store.yaml'
         self.base_path = os.path.dirname(os.path.realpath(__file__)).split('store')[0]
         self.attribute_store = json.load(open(os.path.join(self.base_path, 'store', attribute_store)))
         self.relations_store = yaml.load(open(os.path.join(self.base_path, 'store', relations_store)))
+        self.question_store = yaml.load(open(os.path.join(self.base_path, 'store', question_store)))
         self.rules_store = yaml.load(open(os.path.join(self.base_path, 'store', rules_store)))
 
         # TODO: do we need this?
-        ## Relationship type has basic values 0,1 and 2, whereas the
+        ## Relationship type has basic values 0, 1 and 2, whereas the
         ## rest should be inferred. Like, child + child = 4 = grand
         self.relationship_type = {
             'SO': 1,
@@ -33,5 +36,5 @@ class Store:
             'no-relation': -1
         }
 
-        attr_names = [v["options"] for k,v in self.attribute_store.items()]
+        attr_names = [v["options"] for k, v in self.attribute_store.items()]
         self.attr_names = set([x for p in attr_names for x in p])
